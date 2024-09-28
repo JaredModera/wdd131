3e75a9cbbbc55d1375653e8ae8081ed7d5350773
JaredModera <jareditapablo@gmail.com>
2024-09-28 17:32:08 -0500

    bom seccond commit

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,13 +1,23 @@
-//In your js file, declare three const variables that hold references to the input, button, and .list elements.
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('#list');
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
+button.addEventListener("click", () => {
+    if (input.value.trim() != "") {
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
 
-li.textContent = input.value;
-deleteButton.textContent = '❌';
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
 
-li.append(deleteButton);
-list.append(li);
+        li.append(deleteButton);
+        list.append(li);
+        
+        deleteButton.addEventListener("click", () => {
+            list.removeChild(li);
+        });
+        input.focus();
+
+        input.value = "";
+    }
+});
