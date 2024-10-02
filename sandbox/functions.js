d79a13b8c987c9240b8307e53ccd89e09e90dfe7
JaredModera <jareditapablo@gmail.com>
2024-10-02 18:39:59 -0500

    place page initial commit

---
+++ sandbox/functions.js
@@ -0,0 +1,20 @@
+let firstName = 'Antonia';
+let lastName = 'Francesca';
+
+//REGULAR FUNCTION
+/*function fullName(first, last) {
+    return `${first} ${last}`;
+}
+
+//ANONYMOUS FUNCTION
+const fullName = function (first, last) {
+    return `${first} ${last}`;
+}
+*/
+
+//ARROW FUNCTION
+const fullName = (first, last) =>  `${first} ${last}`;
+
+document.querySelector("#fullName").innerHTML = fullName(firstName, lastName);
+
+console.log(fullName(firstName, lastName));
