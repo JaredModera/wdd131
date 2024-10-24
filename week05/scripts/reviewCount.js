6c411b855764465b7cd93e96d3a571862d689403
JaredModera <jareditapablo@gmail.com>
2024-10-23 19:09:53 -0500

    a

--- week05/scripts/reviewCount.js
+++ week05/scripts/reviewCount.js
@@ -2,5 +2,5 @@ let reviewCount = localStorage.getItem('reviewCount') || 0;
 reviewCount++;
 localStorage.setItem('reviewCount', reviewCount);
 
-document.getElementById('reviewCount').textContent = localStorage.getItem('reviewCount') || 0;  
 //me no entender xd
+document.getElementById('reviewCount').textContent = localStorage.getItem('reviewCount') || 0;  
