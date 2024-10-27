65e13612f9bedc96bf51075e7e8111123b63a545
JaredModera <jareditapablo@gmail.com>
2024-10-27 01:06:52 -0500

    2nd nav almost done

--- project/scripts/discover.js
+++ project/scripts/discover.js
@@ -8,12 +8,13 @@ hamButton.addEventListener("click", () => {
     document.querySelector('h1').classList.toggle('open');
 });
 
+
 const songs = [  
     {   
         songName: "When I'm Sixty-Four",
         author: "The Beatles",
         genre: "Pop",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Dwight Schrute",
         imageUrl:"https://www.billboard.com/wp-content/uploads/2022/03/11.-The-Beatles-%E2%80%98Sgt.-Peppers-Lonely-Hearts-Club-Band-1967-album-art-billboard-1240.jpg?w=768",
         songUrl: "https://open.spotify.com/track/1NrbnHlR2BFREcyWXHIHip"
@@ -22,7 +23,7 @@ const songs = [
         songName: "Something",
         author: "The Beatles",
         genre: "Soft Rock",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Dwight Schrute",
         imageUrl: "https://www.billboard.com/wp-content/uploads/2022/03/2.-The-Beatles-%E2%80%98Abbey-Road-1969-album-art-billboard-1240.jpg?w=768",
         songUrl: "https://open.spotify.com/track/0pNeVovbiZHkulpGeOx1Gj"
@@ -31,7 +32,7 @@ const songs = [
         songName: "I Want You (She's So Heavy)",
         author: "The Beatles",
         genre: "Progressive Rock Blues",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Michael Scott",
         imageUrl: "https://www.billboard.com/wp-content/uploads/2022/03/2.-The-Beatles-%E2%80%98Abbey-Road-1969-album-art-billboard-1240.jpg?w=768",
         songUrl: "https://open.spotify.com/track/3Z25k4ZF6QENy2d9YatsM5"
@@ -40,7 +41,7 @@ const songs = [
         songName: "The Chain",
         author: "Fleetwood Mac",
         genre: "Classic Rock",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Michael Scott",
         imageUrl: "https://www.billboard.com/wp-content/uploads/2022/03/43.-Fleetwood-Mac-%E2%80%98Rumours-1977-album-art-billboard-1240.jpg?w=768",
         songUrl: "https://open.spotify.com/track/7Dm3dV3WPNdTgxoNY7YFnc"
@@ -49,7 +50,7 @@ const songs = [
         songName: "Between the Bars",
         author: "Elliott Smith",
         genre: "Indie Rock",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Pam Beesley",
         imageUrl: "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRC5gyU2xns9poXp75WGyZ3cUrwuw8WmIr86sMyCtnp_0BXfkTcMHhDimGRu3JjDjRMImuoA4-8gVOoOdpb",
         songUrl: "https://open.spotify.com/track/52Bg6oaos7twR7IUtEpqcE"
@@ -58,7 +59,7 @@ const songs = [
         songName: "Caravan",
         author: "John Coltrane",
         genre: "Modal Jazz",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Andy Lightyear",
         imageUrl: "https://upload.wikimedia.org/wikipedia/en/6/66/Whiplash_Soundtrack_Apple_Music.jpg",
         songUrl: "https://open.spotify.com/track/2Xtsv7BUMrNodQWH2JPOc0"
@@ -67,7 +68,7 @@ const songs = [
         songName: "Cheek to Cheek",
         author: "Ella Fitzgerald & Louis Armstrong",
         genre: "Jazz, Traditional Pop",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Tyler The Creator",
         imageUrl: "https://m.media-amazon.com/images/I/816IUoz0hJL._UF1000,1000_QL80_.jpg",
         songUrl: "https://open.spotify.com/track/33jt3kYWjQzqn3xyYQ5ZEh"
@@ -76,7 +77,7 @@ const songs = [
         songName: "Somethin' Stupid",
         author: "Frank Sinatra & Nancy Sinatra",
         genre: "Pop",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Jim Halpert",
         imageUrl: "https://i.scdn.co/image/ab67616d0000b2733e2331ff09f59ea3c87fe5d6",
         songUrl: "https://open.spotify.com/track/4feXcsElKIVsGwkbnTHAfV"
@@ -85,7 +86,7 @@ const songs = [
         songName: "No Moon At All",
         author: "Jonah Jones Quartet",
         genre: "Jazz",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Creed Bratton",
         imageUrl: "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/6b/ea/ee/6beaee9f-a1fd-c75a-b283-d8e5766424aa/00724352455454.jpg/1200x1200bf-60.jpg",
         songUrl: "https://open.spotify.com/track/2Z6EyJu9q72XuPjgG6ZSIu"
@@ -94,7 +95,7 @@ const songs = [
         songName: "Oh Qué Será?",
         author: "Willie Colón",
         genre: "Latin Salsa",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Kevin Malone",
         imageUrl: "https://i1.sndcdn.com/artworks-czUFuvI6qIMopw18-jwlz6g-t500x500.jpg",
         songUrl: "https://open.spotify.com/track/2n1MTLCis6qPSDKdr5XSDI"
@@ -103,7 +104,7 @@ const songs = [
         songName: "Buscando Guayaba",
         author: "Willie Colón & Rubén Blades",
         genre: "Latin Salsa",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Stanley Hudson",
         imageUrl: "https://i.scdn.co/image/ab67616d0000b273d80568f980cdc7cef1f877a6",
         songUrl: "https://open.spotify.com/track/51pnSx6vfBzV8lkLLNFFpy"
@@ -112,7 +113,7 @@ const songs = [
         songName: "Tanto Amor",
         author: "Escarlata & Los Rumberos",
         genre: "Latin Bolero",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Oscar Martinez",
         imageUrl: "https://is1-ssl.mzstatic.com/image/thumb/Music125/v4/55/43/f9/5543f936-ae09-d602-fd81-2f7f581dbfe9/198000948788.jpg/1200x1200bb.jpg",
         songUrl: "https://open.spotify.com/track/2DSBxnRi8Y1Tk4f0Am8pzA"
@@ -121,7 +122,7 @@ const songs = [
         songName: "Don",
         author: "Miranda!",
         genre: "Electro-Pop",
-        review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugit molestiae nihil dolore esse consequuntur blanditiis saepe vitae totam repellendus tempore exercitationem praesentium sint excepturi labore sed. Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
+        review: "Lorem Culpa sit possimus cupiditate tempora, molestiae sint eum placeat voluptatem reiciendis harum iure exercitationem amet perspiciatis nemo recusandae dolorum! Odio praesentium obcaecati asperiores.",
         reviewer: "Ryan Howard",
         imageUrl: "https://i.scdn.co/image/ab67616d00001e0241ca8a8fdfcce2d6ff7f2d05",
         songUrl: "https://open.spotify.com/track/0WajUW5XINeraP2w0F3F8E"
@@ -170,25 +171,29 @@ latinLink.addEventListener("click", () => filterSongsByGenre("latin"));
 
 function createSongsCard(filteredSongs) {
     //set the content of the div to nothing
-    document.querySelector(".song-grid").innerHTML = "";
+    document.querySelector(".songs-grid").innerHTML = "";
     //apply the template only to the filtered items
     filteredSongs.forEach(song => {
         let card = document.createElement("section");
+        let div = document.createElement("div");
         let songName = document.createElement("h3");
         let author = document.createElement("p");
         let genre = document.createElement("p");
         let review = document.createElement("p");
         let reviewer = document.createElement("p");
-        let songLink = document.createElement("a");
         let img = document.createElement("img");
+        let songLink = document.createElement("a");
 
         //now assing the values to the elements we just created
         songName.textContent = song.songName;
         author.innerHTML = `<span class="label">Song author/s:</span> ${song.author}`;
-        genre.innerHTML = `<span class="label">genre:</span> ${song.genre}`;
-        review.innerHTML = `<span class="label">review:</span> ${song.review}`;
-        reviewer.innerHTML = `<span class="label">review by:</span> ${song.reviewer}`;
+        genre.innerHTML = `<span class="label">Genre:</span> ${song.genre}`;
+        review.innerHTML = `<span class="label">Review:</span> ${song.review}`;
+        reviewer.innerHTML = `<span class="label">Reviewed by:</span> ${song.reviewer}`;
         songLink.innerHTML = `<span class="label">Listen On Spotify:</span> ${song.songUrl}`;
+        //sett a class for the div
+        div.setAttribute("class", "songs-div")
+        //sett images attributes
         img.setAttribute("src", song.imageUrl);
         img.setAttribute("alt", `${song.songName}`);
         img.setAttribute("width","200");
@@ -198,31 +203,19 @@ function createSongsCard(filteredSongs) {
         // Set the text content of the link
         songLink.textContent = "Listen to the Song";
         songLink.target = "_blank";
-        card.appendChild(songName);
-        card.appendChild(genre);
-        card.appendChild(author);
-        card.appendChild(review);
-        card.appendChild(reviewer);
-        card.appendChild(songLink);
+
+        div.appendChild(songName);
+        div.appendChild(genre);
+        div.appendChild(author);
+        div.appendChild(review);
+        div.appendChild(reviewer);
+        div.appendChild(songLink);
+
+        card.appendChild(div);
         card.appendChild(img);
 
         //now append the card/section to the div with the class of img-grid
-        document.querySelector(".song-grid").appendChild(card);
+        document.querySelector(".songs-grid").appendChild(card);
     });
 }
 createSongsCard(songs);
-/*const templeTemplate = (temples) => {
-    return `
-    <h3>${temples.templeName}</h3>
-    <p><span class="label">Location:</span>${temples.location}</p>
-    <p><span class="label">Dedicated:</span>${temples.dedicated}</p>
-    <p><span class="label">Size:</span>${temples.area} sq ft</p>
-    <img src="${temples.imageUrl}" alt="${temples.templeName}" loading="lazy">`;
-}
-
-function renderTemple(temples) {
-    const html = temples.map(templeTemplate);
-    document.querySelector(".img-grid").innerHTML = html.join("");
-}
-
-renderTemple(temples);*/ //It was Worth a try :C pipipipipi
